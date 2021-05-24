685046a62a5be2c73093769f3bfb19f7884b7782
NIR <MAILMAIL10@GMAIL.COM>
2021-05-24 12:12:43 +0300

    daily

---
+++ client/src/components/Notifications.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import {Button} from '@material-ui/core';
+
+import { SocketContext } from '../SocketContext';
+
+const Notifications = () => {
+    const { answerCall, call, callAccepted } = useContext(SocketContext);
+
+    return (
+       <>
+
+        { call.isReacievedCall && !callAccepted && (
+            <div style= {{ display: 'flex', justifyContent: 'center' }}>
+                <h1>{call.name} is calling now </h1>
+                <Button variant="contained" color="primary" onClick={answerCall}>
+                    Answer Call
+                </Button>
+            </div>
+        )};
+
+       </>
+    )
+}
+
+export default Notifications
