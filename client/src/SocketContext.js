692a71e082aff7fc5b3f733ca66531c49e7da6ad
NIR <MAILMAIL10@GMAIL.COM>
2021-05-24 13:20:31 +0300

    daily

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('https://vca.netlify.app:5000');
+const socket = io();
 
 const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null);
