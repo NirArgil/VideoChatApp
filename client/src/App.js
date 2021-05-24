90b05525c94f9c7a1c66e6b579ca03d5923d99d3
NIR <MAILMAIL10@GMAIL.COM>
2021-05-24 12:45:38 +0300

    daily

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Typography, AppBar } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import styled from 'styled-components'
 
 //Components
 import VideoPlayer from './components/VideoPlayer'
